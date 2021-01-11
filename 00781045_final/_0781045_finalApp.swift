897d4f90fe40d1fa21a796abd6213385874e38a1
aaaaaaaaaaa <aaaaa>
2021-01-11 19:27:00 +0800

    Initial Commit

---
+++ 00781045_final/_0781045_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0781045_finalApp.swift
+//  00781045_final
+//
+//  Created by User09 on 2021/1/11.
+//
+
+import SwiftUI
+
+@main
+struct _0781045_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
